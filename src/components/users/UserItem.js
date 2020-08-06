dca3637e67ff62fc87257710b4d5012686d1ec05
nullbitcoder <lxxxl1146@gmail.com>
2020-08-06 05:27:34 -0500

    start adding STATE

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  constructor(props) {
+    super();
+    console.log(321);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>User Item</p>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
