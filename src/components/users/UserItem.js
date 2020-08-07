bc7ef8cc9e05229ceb53b0c5211d62e0a7b60d5d
nullbitcoder <lxxxl1146@gmail.com>
2020-08-06 19:06:57 -0500

    testing api structure, class(s) without state, convert to function components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  constructor(props) {
-    super();
-    console.log(321);
-  }
-
-  render() {
-    return (
+// class UserItem extends Component
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  // render() {
+  // destructure state
+  // const { login, avatar_url, html_url } = this.props.user; //no longer class dont need "this"
+  // const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
       <div>
-        <p>User Item</p>
+        <a
+          href={html_url}
+          className="btn btn-dark btn-sm my-1
+          "
+        >
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+  // }
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
