9e3b36d3f50eaec9692e96f578b2abb9eb5afdfa
nullbitcoder <lxxxl1146@gmail.com>
2020-08-09 20:24:03 -0500

    added error checking on search bar, add clear funct, nav bar routing home/about, keep search results in storage while switching between routes

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
     text: '',
   };
+
+  static propTypes = {
+    searchUsrs: PropTypes.func.isRequired,
+    clearUsrs: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -27,6 +43,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
